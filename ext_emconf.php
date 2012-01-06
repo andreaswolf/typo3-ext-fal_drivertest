133ddcaa7c08e11bfff5dc8947cfcde38c4277a9
Andreas Wolf <andreas.wolf@ikt-werk.de>
2012-01-06 17:17:58 +0100

    Initial commit of Kickstarter output

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "fal_drivertest".
+#
+# Auto generated 06-01-2012 17:16
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'FAL driver test',
+	'description' => 'Provides generic tests for all FAL functionality. The test can be run against an arbitrary storage to see if the driver performs as expected.',
+	'category' => 'example',
+	'author' => 'Andreas Wolf',
+	'author_email' => 'andreas.wolf@ikt-werk.de',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"9977";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"c1ef";s:20:"doc/wizard_form.html";s:4:"20d5";}',
+);
+
+?>
